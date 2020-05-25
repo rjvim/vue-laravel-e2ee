cf507f02bc4c386bffd95537f342c71932a9c03a
Rajiv Seelam <rajiv@betalectic.com>
2020-05-25 12:29:00 +0530

    Add openpgp encryption library and take it for a ride

--- resources/js/components/Layout.vue
+++ resources/js/components/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="ready"></div>
+  <div v-if="ready"></div>
 </template>
 
 <script>
@@ -12,43 +12,97 @@ import findUserPublicKey from "../functions/findUserPublicKey.js";
 import CreateNote from "./CreateNote";
 import AllNote from "./AllNote";
 import { EThree } from "@virgilsecurity/e3kit-browser";
+const openpgp = require("openpgp");
 
 export default {
-    data: () => {
-        return {
-            ready: false
-        };
-    },
-    components: {
-        CreateNote,
-        AllNote
-    },
+  data: () => {
+    return {
+      ready: false
+    };
+  },
+  components: {
+    CreateNote,
+    AllNote
+  },
 
-    async mounted() {
-        // First register user
+  async mounted() {
+    // First register user
 
-        // Ask user for password
+    // Ask user for password
 
-        // Backup to virgil
-        appUser = await getUser();
-        let virgilToken = await getVirgilToken();
-        eThree = await initializeEThree(virgilToken);
-        // await registerUser(virgilToken);
-        const hasLocalPrivateKey = await eThree.hasLocalPrivateKey();
+    // Backup to virgil
+    appUser = await getUser();
+    // let virgilToken = await getVirgilToken();
+    // eThree = await initializeEThree(virgilToken);
+    // await registerUser(virgilToken);
+    // const hasLocalPrivateKey = await eThree.hasLocalPrivateKey();
 
-        console.log("hasLocalPrivateKey", hasLocalPrivateKey);
+    // console.log("hasLocalPrivateKey", hasLocalPrivateKey);
 
-        // await unregisterUser();
-        // const publicKeys = await eThree.findUsers([appUser.email]);
+    // await unregisterUser();
+    // const publicKeys = await eThree.findUsers([appUser.email]);
 
-        // const { loginPassword, backupPassword } = await EThree.derivePasswords(
-        //     "password"
-        // );
+    // const { loginPassword, backupPassword } = await EThree.derivePasswords(
+    //     "password"
+    // );
 
-        // console.log(loginPassword, backupPassword);
+    // console.log(loginPassword, backupPassword);
 
-        this.ready = true;
-        Bus.$emit("virgilReady");
+    this.ready = true;
+    Bus.$emit("virgilReady");
+    this.pgp();
+  },
+
+  methods: {
+    async pgp() {
+      const { privateKeyArmored, publicKeyArmored } = await openpgp.generateKey(
+        {
+          curve: "ed25519" // ECC curve name
+        }
+      );
+
+      console.log(privateKeyArmored); // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
+      console.log(publicKeyArmored); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+
+      //   const privateKey = (await openpgp.key.readArmored([privateKeyArmored]))
+      //     .keys[0];
+
+      //   const encrypted = await openpgp.encrypt({
+      //     message: openpgp.message.fromText("Hello World, Rajiv"), // input as Message object
+      //     publicKeys: (await openpgp.key.readArmored(publicKeyArmored)).keys
+      //   });
+
+      //   const ciphertext = encrypted.data;
+
+      //   console.log(ciphertext);
+
+      //   const decrypted = await openpgp.decrypt({
+      //     message: await openpgp.message.readArmored(ciphertext),
+      //     privateKeys: [privateKey]
+      //   });
+
+      //   const plaintext = await openpgp.stream.readToEnd(decrypted.data);
+
+      //   console.log(plaintext);
+
+      //   console.log("encrypt");
+      //   const { message } = await openpgp.encrypt({
+      //     message: openpgp.message.fromText("Hello World, Rajiv"),
+      //     passwords: ["secret stuff"],
+      //     armor: false
+      //   });
+      //   const encrypted = message.packets.write();
+      //   console.log("encrypted", encrypted);
+      //   const { data: decrypted } = await openpgp.decrypt({
+      //     message: await openpgp.message.read(encrypted), // parse encrypted bytes
+      //     passwords: ["secret stuff"]
+      //   });
+      //   console.log(decrypted); // Uint8Array([0x01, 0x01, 0x01])
     }
+  },
+
+  async destroyed() {
+    console.log("Destroyed");
+  }
 };
 </script>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+use Virgil\CryptoImpl\VirgilCardCrypto;
+use Virgil\CryptoImpl\VirgilCrypto;
+use Virgil\Sdk\CardManager;
+use Virgil\Sdk\Web\Authorization\CallbackJwtProvider;
+use Virgil\Sdk\Web\Authorization\TokenContext;
+
+use Virgil\Sdk\Verification\VerifierCredentials;
+use Virgil\Sdk\Verification\VirgilCardVerifier;
+use Virgil\Sdk\Verification\Whitelist;
+use Virgil\CryptoImpl\VirgilAccessTokenSigner;
+use App\Helpers\JWTGenerator;
+
+// "AT.bCah4Y6dOkZ9iKKpXLk9IwayoftMEwN9"
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +29,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $authenticatedQueryToServerSide = function (TokenContext $context) {
+        $JWTGenerator = new JWTGenerator();
+
+        return $JWTGenerator->generate('rajiv@betalectic.com');
+
+        // Get generated token from server-side
+        return "eyJraWQiOiI3MGI0NDdlMzIxZjNhMGZkIiwidHlwIjoiSldUIiwiYWxnIjoiVkVEUzUxMiIsImN0eSI6InZpcmdpbC1qd3Q7dj0xIn0.eyJleHAiOjE1MTg2OTg5MTcsImlzcyI6InZpcmdpbC1iZTAwZTEwZTRlMWY0YmY1OGY5YjRkYzg1ZDc5Yzc3YSIsInN1YiI6ImlkZW50aXR5LUFsaWNlIiwiaWF0IjoxNTE4NjEyNTE3fQ.MFEwDQYJYIZIAWUDBAIDBQAEQP4Yo3yjmt8WWJ5mqs3Yrqc_VzG6nBtrW2KIjP-kxiIJL_7Wv0pqty7PDbDoGhkX8CJa6UOdyn3rBWRvMK7p7Ak";
+    };
+
+    // Setup AccessTokenProvider
+    $accessTokenProvider = new CallbackJwtProvider($authenticatedQueryToServerSide);
+
+    // initialize Crypto library
+    $cardCrypto = new VirgilCardCrypto();
+    $accessTokenSigner = new VirgilAccessTokenSigner();
+
+
+    $privateKeyStr = env('VIRGIL_KEY');
+    $apiKeyData = base64_decode($privateKeyStr);
+
+    // Crypto library imports a private key into a necessary format
+    $crypto = new VirgilCrypto();
+    $privateKey = $crypto->importPrivateKey($apiKeyData);
+
+    $virgilCardVerifier = new VirgilCardVerifier($cardCrypto, true, true);
+
+    $cardManager = new CardManager($cardCrypto, $accessTokenProvider, $virgilCardVerifier);
+
+
+    $cards = $cardManager->searchCards('');
+
+    dd($cards);
+    
     // $extension = 'virgil_crypto_php';
 
     // $result = [
